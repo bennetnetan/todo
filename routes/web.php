b0d989cf2db4d9cb45884b0cbe75b3891173ebb4
BenjaNetan <netanyahubenjamin70@gmail.com>
2026-03-13 13:36:58 +0300

    Redirect dashboard to todos

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return redirect()->route('todos.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 // application routes protected by auth
